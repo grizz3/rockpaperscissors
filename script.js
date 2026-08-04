18e5e52656d15629849838c48983396fb8c6a551
grizz3 <grizzly476@outlook.com>
2026-08-04 15:32:11 -0500

    Got game logic working. Decided to change to strings and check arguments with strings instead of converting player input into numbers like TOP seemed to want. Much more readable code in this case.

--- script.js
+++ script.js
@@ -1,39 +1,64 @@
-console.log("Hello World")
-
-let computerChoice = 0;
-let humanChoice = 0;
-
 let humanScore = 0;
 let computerScore = 0;
 
-function getComputerChoice(){
-    return Math.floor(Math.random() * 3);
+let computerChoice = "";
+let humanChoice = "";
+
+function getComputerChoice() {
+    let randomNum = Math.floor(Math.random() * 3);
+    let choice = "none";
+    if (randomNum == 0) {
+        choice = "rock";
+        return choice;
+    }
+    else if (randomNum == 1) {
+        choice = "paper";
+        return choice;
+    }
+    else if (randomNum == 2) {
+        choice = "scissors";
+        return choice;
+    }
 }
 
-
-function getHumanChoice(){
+function getHumanChoice() {
     let input = prompt("Rock, paper, or scissors?");
-    input = input.toLowerCase();
-    if (input == "rock"){
-        input = 0;
-        return input;
-    }
-    else if(input == "paper"){
-        input = 1;
-        return input;
-    }
-    else if(input == "scissors"){
-        input = 2;
-        return input;
-    }
+    return input.toLowerCase();
 }
 
-function playRound(humanChoice, computerChoice){
-    computerChoice = getComputerChoice();
+function playRound(){
     humanChoice = getHumanChoice();
-    return "playRound";
+    computerChoice = getComputerChoice();
+
+    if (humanChoice == computerChoice){
+        return "Tie."
+    }
+    else if(humanChoice == "rock"){
+        if (computerChoice == "scissors"){
+            return "You win!";
+        }
+        else{
+            return "You lose.";
+        }
+    }
+    else if(humanChoice == "paper"){
+        if (computerChoice == "rock"){
+            return "You win!";
+        }
+        else{
+            return "You lose.";
+        }
+    }
+    else if(humanChoice == "scissors"){
+        if (computerChoice == "paper"){
+            return "You win!";
+        }
+        else{
+            return "You lose.";
+        }
+    }
 }
-
-
 
 console.log(playRound());
+console.log("humanChoice = " + humanChoice);
+console.log("computerChoice = " + computerChoice);
