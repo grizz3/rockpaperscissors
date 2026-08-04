cf2cd5a14666d525f4fca9676621333ef0ee20fe
grizz3 <grizzly476@outlook.com>
2026-08-04 15:46:57 -0500

    Created a 5 time loop of playRound, incrementing rounds and wins/losses, for a final result log once round count is 5.

--- script.js
+++ script.js
@@ -35,30 +35,51 @@ function playRound(){
     }
     else if(humanChoice == "rock"){
         if (computerChoice == "scissors"){
+            humanScore++;
             return "You win!";
         }
         else{
+            computerScore++;
             return "You lose.";
         }
     }
     else if(humanChoice == "paper"){
         if (computerChoice == "rock"){
+            humanScore++;
             return "You win!";
         }
         else{
+            computerScore++;
             return "You lose.";
         }
     }
     else if(humanChoice == "scissors"){
         if (computerChoice == "paper"){
+            humanScore++;
             return "You win!";
         }
         else{
+            computerScore++;
             return "You lose.";
         }
     }
 }
 
-console.log(playRound());
-console.log("humanChoice = " + humanChoice);
-console.log("computerChoice = " + computerChoice);
+function playGame(){
+    for (let round = 0; round < 5; round++){
+        console.log(playRound());
+    }
+
+    if (humanScore > computerScore) {
+        console.log("You win the game " + humanScore + " : " + computerScore);
+    } else if (humanScore < computerScore) {
+        console.log("You lose the game " + humanScore + " : " + computerScore);
+    }
+    else {
+        console.log("Tie game " + humanScore + " : " + computerScore);
+    }
+}
+
+playGame();
+//console.log("humanChoice = " + humanChoice);
+//console.log("computerChoice = " + computerChoice);
